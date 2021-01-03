109e7a5282c91a51a2d3710d759c1ab414a46b15
Kristof Petho <kristof.petho@gmail.com>
2021-01-03 16:20:20 +0100

    techpack: dsi: remove stack corruption source caused by OnePlus code
    [@0ctobot: Squash with kristofpetho/kernel_oneplus_sm8250@e2e7a6c
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- techpack/display/msm/sde/sde_encoder_phys_cmd.c
+++ techpack/display/msm/sde/sde_encoder_phys_cmd.c
@@ -1846,16 +1846,6 @@ static void _sde_encoder_autorefresh_disable_seq2(
 				autorefresh_status, SDE_EVTLOG_FUNC_CASE2);
 	}
 
-	if (!(autorefresh_status & BIT(7))) {
-		usleep_range(AUTOREFRESH_SEQ2_POLL_TIME,
-			AUTOREFRESH_SEQ2_POLL_TIME + 1);
-
-		autorefresh_status = hw_mdp->ops.get_autorefresh_status(hw_mdp,
-					phys_enc->intf_idx);
-		SDE_EVT32(DRMID(phys_enc->parent), phys_enc->intf_idx - INTF_0,
-				autorefresh_status, SDE_EVTLOG_FUNC_CASE2);
-	}
-
 	while (autorefresh_status & BIT(7)) {
 		if (!trial) {
 			SDE_ERROR_CMDENC(cmd_enc,
